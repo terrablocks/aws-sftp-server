65dff46aebbc533de7b9125a73fbb706dbdf3406
Simon Staszkiewicz <simonstaszkiewicz@gmail.com>
2022-03-27 11:34:23 -0700

    Fix for non-endpoint type length check issue

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_transfer_server" "this" {
   certificate   = var.certificate_arn
 
   dynamic "endpoint_details" {
-    for_each = length(var.endpoint_details) == 0 ? [] : [var.endpoint_details]
+    for_each = var.endpoint_details == null ? [] : [var.endpoint_details]
     content {
       vpc_id                 = lookup(endpoint_details.value, "vpc_id", null)
       subnet_ids             = lookup(endpoint_details.value, "subnet_ids", null)
